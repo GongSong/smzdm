a7f33ad3a7930b0734600f64e7719f5c030e79bb
realeve@qq.com <lb159753>
2017-03-30 22:27:25 +0800

    删除一些不必要的文件

--- server/conf/session.js
+++
@@ -1,5 +0,0 @@
-const SESSION_SECRET = 'Um7qM9jvFeRvHsYheu0ZgGmzl1Z1p67PhwqtnNIrkTg';
-
-module.exports = {
-  sessionSecret: SESSION_SECRET
-};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var util = require('../common');
 var assertsDir = require('../conf/app').assertsDir;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //使用index.html时忽略权限校验
-  //将接口校验规则写至cookies用于全局数据读取
-  util.setToken2Cookies(req,res,true);
-  util.handleAuthCheck(req, res, assertsDir + '/home.html');
+  res.sendFile(assertsDir + '/home.html');
 });
 
 module.exports = router;
