32a1d19dd4254ef8e516b03f9f6b119e40f319d2
realeve <realeve@qq.com>
2017-03-31 00:09:45 +0800

    去掉全局变量

--- controller/crawler.js
+++ controller/crawler.js
@@ -1,30 +1,27 @@
 var https = require('https');
 var cheerio = require('cheerio');
 
-var goods = [];
-
 function htmlParser(html) {
     var $ = cheerio.load(html);
+    var goods = [];
     if ($('li').hasClass('text-center empty-list')) {
-        return 0;
-    } else {
-        $('.js-goods-card.goods-card.card').each(function(idx, e) {
-            let goodElement = { alias: '', goodId: '', title: '', price: '', priceTaobao: '', imgSrc: '', isVirtual: '' };
-            $(this).find($('.js-goods-buy.buy-response')).each(function(i, element) {
-                goodElement.title = $(this).attr('data-title');
-                goodElement.alias = $(this).attr('data-alias');
-                goodElement.price = parseFloat($(this).attr('data-price'));
-                goodElement.goodId = parseInt($(this).attr('data-id'));
-                goodElement.isVirtual = parseInt($(this).attr('data-isvirtual'));
-            });
-            goodElement.imgSrc = $(this).find($('.goods-photo.js-goods-lazy')).data('src');
-            let txt = $(this).find($('.goods-price-taobao')).text();
-            goodElement.priceTaobao = (txt === null || txt.length === 0) ? 0 : parseFloat(txt.substr(3, txt.length - 3));
-            goods.push(goodElement);
-        });
-
-        return 1;
+        return goods;
     }
+    $('.js-goods-card.goods-card.card').each(function(idx, e) {
+        let goodElement = { alias: '', goodId: '', title: '', price: '', priceTaobao: '', imgSrc: '', isVirtual: '' };
+        $(this).find($('.js-goods-buy.buy-response')).each(function(i, element) {
+            goodElement.title = $(this).attr('data-title');
+            goodElement.alias = $(this).attr('data-alias');
+            goodElement.price = parseFloat($(this).attr('data-price'));
+            goodElement.goodId = parseInt($(this).attr('data-id'));
+            goodElement.isVirtual = parseInt($(this).attr('data-isvirtual'));
+        });
+        goodElement.imgSrc = $(this).find($('.goods-photo.js-goods-lazy')).data('src');
+        let txt = $(this).find($('.goods-price-taobao')).text();
+        goodElement.priceTaobao = (txt === null || txt.length === 0) ? 0 : parseFloat(txt.substr(3, txt.length - 3));
+        goods.push(goodElement);
+    });
+    return goods;
 }
 
 async function page_filter(res, shopid, page = 1) {
@@ -37,10 +34,10 @@ async function page_filter(res, shopid, page = 1) {
         });
         response.on('end', function() {
             let result = htmlParser(html);
-            if (result === 0) {
+            if (result.length === 0) {
                 console.log('没有更多结果');
                 console.log('end');
-                res.json(goods);
+                res.json(result);
                 res.end();
                 return '没有更多结果';
             } else {
