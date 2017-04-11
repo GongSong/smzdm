f2e74df354fda9bf3926adee89efc7590f53b97f
musicoooool <musicoooool@gmail.com>
2017-04-12 01:57:10 +0800

    测试抓取chinagold库存

--- controller/crawler.js
+++ controller/crawler.js
@@ -2,10 +2,12 @@ let youzan = require('./shop/youzan');
 let wfx = require('./shop/wfx');
 let ccgold = require('./shop/ccgold');
 let cncoin = require('./shop/cncoin');
+let cncointest = require('./shop/cncoinStorage');
 
 module.exports = {
     youzan,
     wfx,
     ccgold,
-    cncoin
+    cncoin,
+    cncointest
 };
