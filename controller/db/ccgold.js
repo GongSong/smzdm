7631b070fcb6e8bc1200b01dd4af93d35e6e6456
musicoooool <musicoooool@gmail.com>
2017-04-13 01:11:57 +0800

    ccgold数据入库

--- controller/db/ccgold.js
+++ controller/db/ccgold.js
@@ -2,28 +2,40 @@ let query = require('../../schema/mysql.js')
 let sqlParser = require('../util/sqlParser')
 let sql = require('../../schema/sql.js')
 
+async function insertData(sql) {
+    return await query(sql, function (result) {
+        let string = JSON.stringify(result)
+        return string;
+    });
+}
+
 function setGoodList(req, res, next) {
     let arr = require('../data/ccgoldGoodsList.json')
-    let spiderData = {
-        data: arr
-    }
-    let sqlStr = sqlParser.handelCcgoldGoodsList(spiderData)
-    query(sqlStr, function (result) {
-        let str = JSON.stringify(result)
-        res.send(str)
-    })
+    
+    let promises = [];
+    arr.forEach(function (element) {
+        promises.push(insertData(sqlParser.handelCcgoldGoodsList(element)));
+    }, this);
+
+    Promise.all(promises).then(item => {
+        res.json({
+            msg: '共' + promises.length + ' 条数据插入完毕'
+        });
+    });
 }
 
 function setGoodDetail(req, res, next) {
-    let arr = require('../data/ccgoldGoodsDetail.json')
-    let spiderData = {
-        data: arr
-    }
-    let sqlStr = sqlParser.handelCcgoldGoodsDetail(spiderData)
-    query(sqlStr, function (result) {
-        let str = JSON.stringify(result)
-        res.send(str)
-    })
+    let arr = require('../data/ccgoldDetail.json')
+    let promises = [];
+    arr.forEach(function (element) {
+        promises.push(insertData(sqlParser.handelCcgoldGoodsDetail(element)));
+    }, this);
+
+    Promise.all(promises).then(item => {
+        res.json({
+            msg: '共' + promises.length + ' 条数据插入完毕'
+        });
+    });
 }
 
 module.exports = {
