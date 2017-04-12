91daf7f2df496d393985fac7af9df59966804a9b
musicoooool <musicoooool@gmail.com>
2017-04-13 00:45:56 +0800

    ccgold数据入库

--- controller/db/ccgold.js
+++ controller/db/ccgold.js
@@ -2,18 +2,6 @@ let query = require('../../schema/mysql.js')
 let sqlParser = require('../util/sqlParser')
 let sql = require('../../schema/sql.js')
 
-function setStockData(req, res) {
-    let arr = require('../data/wfx_shenyang.json')
-    let spiderData = {
-        data: arr
-    }
-    let sqlStr = sqlParser.handleWfxStockData(spiderData)
-    query(sqlStr, function(result) {
-        let str = JSON.stringify(result)
-        res.send(str)
-    })
-}
-
 function setGoodList(req, res, next) {
     let arr = require('../data/ccgoldGoodsList.json')
     let spiderData = {
@@ -39,6 +27,6 @@ function setGoodDetail(req, res, next) {
 }
 
 module.exports = {
-    setStockData,
-    setGoodList
+    setGoodList,
+    setGoodDetail
 }
