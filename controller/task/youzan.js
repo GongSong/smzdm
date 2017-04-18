1c49709a3e1e79e777a684e9da0f8f7534a49373
realeve <realeve@qq.com>
2017-04-19 00:00:47 +0800

    有赞数据增量同步完毕，增量数据测试待系统升级后完成

--- controller/task/youzan.js
+++ controller/task/youzan.js
@@ -1,24 +1,45 @@
 let read = require('../shop/youzan');
 let save = require('../db/youzan');
-let needUpdate = require('./db').needUpdate;
+let db = require('./db');
 
 async function init() {
-    // let flag = await needUpdate('ccgold_goods_detail');
+    let flag, idx = 0;
+    console.log('\n\n正在同步youzan');
+    let goodsList;
+    flag = await db.needUpdate('yz_goods');
+    if (flag) {
+        console.log(`${++idx}.开始同步商品列表.`);
+        goodsList = await read.getGoodsByShopName();
+        await save.setGoodsData(goodsList);
+        await db.setCrawlerStatus('yz_goods');
+    }
 
-    // console.log('\n正在同步youzan');
-    // console.log('B.开始上传ccgold每日数据');
-    // if (flag) {
-    //     asyncData();
-    //     return;
-    // }
-    // console.log('今日数据已上传，无需重复采集!\n');
+    flag = await db.needUpdate('yz_stock');
+    if (flag) {
+        console.log(`${++idx}.开始同步库存信息.`);
+        let storage = await read.getSaleInfo(goodsList);
+        await save.setStockData(detail);
+        await db.setCrawlerStatus('yz_stock');
+    }
+
+    flag = await db.needUpdate('yz_trade_record');
+    if (flag) {
+        console.log(`${++idx}.开始同步交易记录.`);
+        let trades = await read.getTradRecord(goodsList);
+        await save.setSaleDetail(trades);
+        await db.setCrawlerStatus('yz_trade_record');
+    }
+    console.log('wfx数据同步完毕\n');
 }
 
-async function asyncData() {
-    // let goodsList = await read.getGoodsList();
-    // await save.setGoodList(goodsList);
+async function loadDefault() {
+    // 商品列表信息/库存信息，该数据每日更新
+    await save.setGoodsData();
+    await save.setStockData();
+    await save.setSaleDetail();
 }
 
 module.exports = {
-    init
+    init,
+    loadDefault
 };
