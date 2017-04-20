05d12c3a6375e11675edab697f98dbabcee99a5f
musicoooool <musicoooool@gmail.com>
2017-04-20 16:34:44 +0800

    去掉jd.sql中注释

--- controller/task.js
+++ controller/task.js
@@ -27,7 +27,7 @@ async function loadDefault() {
 
     // ccgold 无需初始化载入数据，直接加载即可
 
-    // cncoin.loadDefault();
+    await cncoin.loadDefault();
 
     // wfx.loadDefault();
 
--- schema/sqlInit/jd.sql
+++ schema/sqlInit/jd.sql
@@ -1,23 +1,4 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : MySQL
-Source Server Version : 50621
-Source Host           : 127.0.0.1:3306
-Source Database       : smzdm
-
-Target Server Type    : MYSQL
-Target Server Version : 50621
-File Encoding         : 65001
-
-Date: 2017-04-20 10:51:30
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for jd_goods
--- ----------------------------
 DROP TABLE IF EXISTS `jd_goods`;
 CREATE TABLE `jd_goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
